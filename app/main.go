8ecc2cb7bbf6571c461130b8e0b7b8758f17d40f
FhmiSddq <fahmifx72@gmail.com>
2025-11-21 20:27:51 +0700

    Update main.go

--- app/main.go
+++ app/main.go
@@ -1,7 +1,7 @@
 // Package app calls the bootstrap package
 package main
 
-import "https://github.com/FhmiSddq/ProyekJarkom/internal/app/bootstrap"
+import "github.com/FhmiSddq/ProyekJarkom/internal/app/bootstrap"
 
 func main() {
 	bootstrap.Start()
