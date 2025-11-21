c0ba389ef38539d96101464ef8430b1d2c43a226
FhmiSddq <fahmifx72@gmail.com>
2025-11-21 20:24:16 +0700

    Create chat.go

---
+++ internal/app/chat/handler/chat.go
@@ -0,0 +1,125 @@
+// Package handler starts and handles incoming and outgoing messages
+package handler
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+
+	"https://github.com/FhmiSddq/ProyekJarkom/internal/infra/env"
+)
+
+type ChatHandlerItf interface {
+	RequestConnection(address string)
+	Announce(message string)
+	Listen()
+	HandleConnection(connection net.Conn)
+	CloseConnection(connection net.Conn)
+	GetConnection(address string) net.Conn
+}
+
+type ChatHandler struct {
+	env         *env.Env
+	connections map[net.Conn]string
+}
+
+func New(env *env.Env) ChatHandlerItf {
+	ChatHandler := ChatHandler{
+		env:         env,
+		connections: make(map[net.Conn]string),
+	}
+
+	return &ChatHandler
+}
+
+func (c *ChatHandler) RequestConnection(address string) {
+	connection, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	log.Printf("tcp connection to host %s success\n", address)
+
+	c.connections[connection] = address
+
+	go c.HandleConnection(connection)
+}
+
+func (c *ChatHandler) Announce(message string) {
+	for connection := range c.connections {
+		fmt.Fprintln(connection, message)
+	}
+}
+
+func (c *ChatHandler) Listen() {
+	listenAddress := fmt.Sprintf(":%s", strconv.Itoa(int(c.env.Port)))
+
+	listener, err := net.Listen("tcp", listenAddress)
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+
+			continue
+		}
+
+		c.connections[connection] = listenAddress
+
+		go c.HandleConnection(connection)
+	}
+}
+
+func (c *ChatHandler) HandleConnection(connection net.Conn) {
+	connectionName := connection.RemoteAddr().String()
+
+	for {
+		message, err := bufio.NewReader(connection).ReadString('\n')
+		message = strings.TrimSpace(message)
+
+		if err != nil {
+			if err == io.EOF {
+				log.Printf("disconnected from host: %s\n", connectionName)
+			} else {
+				log.Println(err)
+			}
+
+			break
+		}
+
+		log.Printf("[%s]: %s\n", connectionName, message)
+	}
+
+	c.CloseConnection(connection)
+}
+
+func (c *ChatHandler) CloseConnection(connection net.Conn) {
+	connectionName := connection.RemoteAddr().String()
+
+	log.Printf("stopped handling connection: %s\n", connectionName)
+
+	delete(c.connections, connection)
+
+	connection.Close()
+}
+
+func (c *ChatHandler) GetConnection(address string) net.Conn {
+	for connection := range c.connections {
+		if c.connections[connection] == address {
+			return connection
+		}
+	}
+
+	return nil
+}
